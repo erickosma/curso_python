84a8d4417b2c0b23bd85cb81ae0f69f47356ef1f
Erick Giorgio <erick.silva@hotmart.com>
2022-10-06 09:14:17 -0300

    Accounts methods

--- src/account.py
+++ src/account.py
@@ -1,8 +1,21 @@
 class Account:
 
-    def __init__(self, number, owner, balance, limit):
-        print("Construindo objeto...{}".format(self))
-        self.number = number
-        self.owner = owner
-        self.balance = balance
-        self.limit = limit
+    def __init__(self, number, holder, balance, limit):
+        print("Construindo objeto ... {}".format(self))
+        self.__number = number
+        self.__holder = holder
+        self.__balance = balance
+        self.__limit = limit
+
+    def statement(self):
+        return "Saldo de {} do titular {}".format(self.__balance, self.__holder)
+
+    def deposit(self, valor):
+        self.__balance += valor
+
+    def take_out(self, valor):
+        self.__balance -= valor
+
+    def transfer(self, valor, destino):
+        self.take_out(valor)
+        destino.deposit(valor)
--- test/account_test.py
+++ test/account_test.py
@@ -9,6 +9,14 @@ class AccountTestCase(unittest.TestCase):
         account = Account(123, "Eu Ddono", 55.5, 1000.0)
         self.assertEqual(123, account.number)
 
+    def test_account_transfer(self):
+        nico_account = Account(123, "Nico", 55.5, 1000.0)
+        marcos_account = Account(321, "Marcos", 100.0, 1000.0)
+
+        nico_account.transfer(10.0, marcos_account)
+        self.assertEqual("Saldo de 45.5 do titular Nico", nico_account.statement())
+        self.assertEqual("Saldo de 110.0 do titular Marcos", marcos_account.statement())
+
 
 if __name__ == '__main__':
     unittest.main()
